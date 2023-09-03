072e57a76504ee9540e559184db037ef0ea29219
Andy <andrewwang134@gmail.com>
2023-09-04 01:18:05 +1000

    added ttl storage of active tokens

--- backend/src/auth/storeToken.js
+++ backend/src/auth/storeToken.js
@@ -1,34 +1,24 @@
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient } from "mongodb";
 import "dotenv/config";
 
 const client = new MongoClient("mongodb://localhost:27017");
 
 const dbName = "mydb";
 
-export async function storeActiveToken(userId, token) {
+export async function storeActiveToken(token) {
   await client.connect();
   const db = client.db(dbName);
-  const collection = db.collection("users");
-  const tokenStorage = process.env.TOKENID;
-  const IdObj = new ObjectId(tokenStorage);
-  const userStr = userId.toString();
-  const Tokens = await collection.find({ _id: IdObj }).toArray();
-  const filteredTokens = Tokens[0].activeTokens.filter(
-    (currToken) => currToken.userId.toString() === userStr
-  );
-  if (filteredTokens.length === 0) {
-    await collection.updateOne(
-      { _id: IdObj },
-      {
-        $push: {
-          activeTokens: {
-            userId: userId,
-            token: token,
-            expiresAt: { $date: "2024-09-01T16:00:00Z" },
-          },
-        },
-      }
-    );
-  }
+  const collection = db.collection("activeTokens");
+  // const Tokens = await collection.find({}).toArray();
+  // const filteredTokens = Tokens[0].activeTokens.filter(
+  //   (currToken) => currToken.userId.toString() === userStr
+  // );
+  // if (filteredTokens.length === 0) {
+  // RUN ONCE
+  await collection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 3600 });
+  await collection.insertOne({
+    token: token,
+    createdAt: new Date(),
+  });
   return;
 }
