21d18541bee27ab0e6607801e22a13e88d1c4bd7
Andy <andrewwang134@gmail.com>
2024-01-02 01:24:57 +1100

    altered cors headers

--- frontend/src/api/Loginapi.jsx
+++ frontend/src/api/Loginapi.jsx
@@ -8,9 +8,7 @@ export const login = async (username, password) => {
   const response = await todosApi.post("/login", {
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Headers":
-        "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-      "Access-Control-Allow-Methods": "OPTIONS,POST",
+      "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Credentials": true,
       "Access-Control-Allow-Origin": "*",
       "X-Requested-With": "*",
