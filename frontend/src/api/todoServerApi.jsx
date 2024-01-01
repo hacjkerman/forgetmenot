3c046b43860bf85d03c53a62b14edb4a1ced0623
Andy <andrewwang134@gmail.com>
2024-01-02 01:03:50 +1100

    added forgetmenot logo and more cors headers

--- frontend/src/api/todoServerApi.jsx
+++ frontend/src/api/todoServerApi.jsx
@@ -1,7 +1,14 @@
 import axios from "axios";
-axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 export const todosApi = axios.create({
   baseURL: process.env.REACT_APP_TODOSERVER,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers":
+      "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+    "Access-Control-Allow-Methods": "OPTIONS,POST",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Origin": "*",
+    "X-Requested-With": "*",
+  },
 });
