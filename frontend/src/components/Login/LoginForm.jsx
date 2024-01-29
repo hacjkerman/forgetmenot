0a767ac3bab8e21c061ec66e4f824951aecff08e
Andy <andrewwang134@gmail.com>
2024-01-29 16:40:40 +1100

    added google login feature

--- frontend/src/components/Login/LoginForm.jsx
+++ frontend/src/components/Login/LoginForm.jsx
@@ -6,6 +6,8 @@ import password_icon from "./Assets/password.png";
 import { login } from "../../api/Loginapi.jsx";
 import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 
 const notify = (message) => toast(message);
 
@@ -50,6 +52,10 @@ export default function LoginForm(props) {
     }
   };
 
+  const handleGoogleLogin = (credentialResponse) => {
+    console.log(credentialResponse);
+  };
+
   const loginUser = async (user, password) => {
     try {
       const response = await login(user, password);
@@ -75,54 +81,63 @@ export default function LoginForm(props) {
     <>
       <Toaster />
       <form onSubmit={handleSubmit(onSubmit)} className={LoginCSS.container}>
-        <div className={LoginCSS.header}>
-          <div className={LoginCSS.text}>Login</div>
-          <div className={LoginCSS.underline}></div>
-        </div>
-        <div className={LoginCSS.inputs}>
-          <div className={LoginCSS.input}>
-            <img src={email_icon} alt="" />
-
-            <input
-              type="text"
-              placeholder="Username"
-              {...register("username", { required: true, maxLength: 100 })}
-            />
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENTID}>
+          <div className={LoginCSS.header}>
+            <div className={LoginCSS.text}>Login</div>
+            <div className={LoginCSS.underline}></div>
           </div>
-          {showPassword ? (
+          <div className={LoginCSS.inputs}>
             <div className={LoginCSS.input}>
-              <img src={password_icon} alt="" />
+              <img src={email_icon} alt="" />
+
               <input
                 type="text"
-                placeholder="Password"
-                id="password"
-                {...register("password", { required: true, maxLength: 100 })}
+                placeholder="Username"
+                {...register("username", { required: true, maxLength: 100 })}
               />
             </div>
-          ) : (
-            <div className={LoginCSS.input}>
-              <img src={password_icon} alt="" />
-              <input
-                type="password"
-                placeholder="Password"
-                id="password"
-                {...register("password", { required: true, maxLength: 100 })}
-              />
+            {showPassword ? (
+              <div className={LoginCSS.input}>
+                <img src={password_icon} alt="" />
+                <input
+                  type="text"
+                  placeholder="Password"
+                  id="password"
+                  {...register("password", { required: true, maxLength: 100 })}
+                />
+              </div>
+            ) : (
+              <div className={LoginCSS.input}>
+                <img src={password_icon} alt="" />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  id="password"
+                  {...register("password", { required: true, maxLength: 100 })}
+                />
+              </div>
+            )}
+
+            <div className={LoginCSS.password_button}>
+              <input type="checkbox" onClick={toggleShowPassword} />
+              <p>Show Password</p>
             </div>
-          )}
 
-          <div className={LoginCSS.password_button}>
-            <input type="checkbox" onClick={toggleShowPassword} />
-            <p>Show Password</p>
+            <div className={LoginCSS.submit_container}>
+              <input type="submit" value="Login" className={LoginCSS.submit} />
+              <GoogleLogin
+                className={LoginCSS.googleLogin}
+                onSuccess={handleGoogleLogin}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+              <button className={LoginCSS.register} onClick={handleRegister}>
+                Sign Up?
+              </button>
+            </div>
           </div>
-
-          <div className={LoginCSS.submit_container}>
-            <input type="submit" value="Login" className={LoginCSS.submit} />
-            <button className={LoginCSS.register} onClick={handleRegister}>
-              Sign Up?
-            </button>
-          </div>
-        </div>
+        </GoogleOAuthProvider>
       </form>
     </>
   );
