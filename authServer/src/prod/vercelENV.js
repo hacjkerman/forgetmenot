7f6869f63e67d483102dd4e633a650587a16d969
Andy <andrewwang134@gmail.com>
2024-04-03 17:19:34 +1100

    altered auth and todo server address using vercel rest api

--- authServer/src/prod/vercelENV.js
+++ authServer/src/prod/vercelENV.js
@@ -2,35 +2,35 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "../../.env" });
 
-const env = await fetch(
-  "https://api.vercel.com/v9/projects/prj_SdEWmHGS9xYPKBVS5NHMgqcAlYUJ/env",
-  {
-    headers: {
-      Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
-    },
-    method: "get",
-  }
-).then((data) => {
-  return data.json();
-});
-const todo = env.envs.map(
-  (variable) => variable.key === "REACT_APP_TODOSERVER"
-);
-console.log(todo);
-await fetch(
-  "https://api.vercel.com/v9/projects/prj_SdEWmHGS9xYPKBVS5NHMgqcAlYUJ/env/XMbOEya1gUUO1ir4?teamId=SOME_STRING_VALUE",
-  {
-    body: {
-      comment: "database connection string for production",
-      gitBranch: "feature-1",
-      key: "GITHUB_APP_ID",
-      target: "[preview]",
-      type: "plain",
-      value: "bkWIjbnxcvo78",
-    },
-    headers: {
-      Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
-    },
-    method: "patch",
-  }
-);
+export default async function changeEnv(authAddr, todoAddr) {
+  await fetch(
+    `https://api.vercel.com/v10/projects/${process.env.VERCEL_PROJECT}/env?upsert=true`,
+    {
+      body: JSON.stringify({
+        key: "REACT_APP_AUTHSERVER",
+        value: authAddr,
+        type: "encrypted",
+        target: ["preview", "production", "development"],
+      }),
+      headers: {
+        Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
+      },
+      method: "post",
+    }
+  );
+  await fetch(
+    `https://api.vercel.com/v10/projects/${process.env.VERCEL_PROJECT}/env?upsert=true`,
+    {
+      body: JSON.stringify({
+        key: "REACT_APP_TODOSERVER",
+        value: todoAddr,
+        type: "encrypted",
+        target: ["preview", "production", "development"],
+      }),
+      headers: {
+        Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
+      },
+      method: "post",
+    }
+  );
+}
