dcbded8c9ed5c8ef03780a1c004dcfdde2c3bf24
Andy <andrewwang134@gmail.com>
2024-04-29 22:59:14 +1000

    added colour wheel boilerplate

---
+++ frontend/src/colourWheel/colours.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Colours() {
+  return (
+    <div style={{ color: `#2304ef`, width: "500px", heigh: "200px" }}>
+      Hello
+    </div>
+  );
+}
+
+export default Colours;
