82f23997476fa62a2e7291186af6983cb109e3e4
Andrew <z5361281@ad.unsw.edu.au>
2024-08-08 14:11:52 +1000

    added changes to board and new daily checkbox feature

--- frontend/src/helpers/offlineMethods/todoMethods.jsx
+++ frontend/src/helpers/offlineMethods/todoMethods.jsx
@@ -1,1 +1,12 @@
+import { addListener } from "../../components/DailyListener/DailyListener";
+
+export const offAddTodo = async (newTodo, column, columns) => {
+  if (newTodo.daily) {
+    addListener(newTodo);
+  }
+  columns[column].todos.push(newTodo);
+  console.log(columns);
+  columns.todoIndex++;
+  localStorage.setItem("todos", JSON.stringify(columns));
+};
 export class offlineTodoMethods {}
